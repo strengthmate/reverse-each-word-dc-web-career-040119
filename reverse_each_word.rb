98d7edd81cbd41e11fbcf1fb5edcec2689cf3175
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 13:40:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(string)
   
-string.delete(",")
+deletion = string.delete(",")
   
-arr = string.split(' ')
-
-.collect{|arr| arr.reverse}
+arr = deletion.split(' ,').collect{|arr| arr.reverse}
 
     
 end

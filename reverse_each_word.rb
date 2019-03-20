3c5a3b167968e6c1229ea894e0542761d220154a
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 13:48:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
-
-array = string.split(' ')
-okay = array.collect{|new| new.reverse}
-return okay.join(' ')
-    
+  array = string.split(' ')
+  
+  (array.collect{|new| new.reverse}).join(' ')
+  
+  # okay = array.collect{|new| new.reverse}
+  # return okay.join(' ')
 end
